4374608e1a2b8d3abb0cea74ab1b2197e4b9699c
Mark Voorberg <mark@binaryops.ca>
2018-03-29 17:25:50 -0700

    adding cookie to keep a uq name

--- js/main.js
+++ js/main.js
@@ -18,6 +18,41 @@ WaveManager = new WaveManager(game);
 
 document.addEventListener("mousewheel", function(e){ InputManager.handleMouseWheel(e); }, false);
 
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+			c = c.substring(1, c.length);
+		}
+        if (c.indexOf(nameEQ) == 0) {
+			return c.substring(nameEQ.length, c.length);
+		}
+    }
+    return null;
+}
+
+var rand = getCookie('uq');
+if (!rand) {
+    rand = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    setCookie('uq', rand, 365);
+}
+// starting! Record game starts...
+
+
+
 game.state.start('Boot');
 
 window.game = game;

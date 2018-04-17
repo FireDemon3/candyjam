9c64905b6bbd35f6f45007b1af8145500542d084
FireDemon3 <Lucas.Voorberg36@gmail.com>
2018-04-16 20:43:05 -0700

    edited addicting 2 tileset

--- js/resources.js
+++ js/resources.js
@@ -103,6 +103,7 @@ MainGame.resources.LoaderState = {
 		, {name: 'baddie_die_sfx', path: assetsFolder + '/audio/baddie_die_sfx.wav'}
 		, {name: 'baddie_boss_die_sfx', path: assetsFolder + '/audio/baddie_boss_die.wav'}
 		, {name: 'player_hurt_sfx', path: assetsFolder + '/audio/player_hurt.wav'}
+		, {name: 'victory_sfx', path: assetsFolder + '/audio/victory.wav'}
 		/*, {name: 'menu_music', path: assetsFolder + '/audio/spiff_tune_to_the_moon.mp3'}*/
 		, {name: 'game_music', path: assetsFolder + ( MainGame.addictingMode ? '/audio/run_music.mp3' : '/audio/spiff_tune_to_the_moon.mp3')}
 	]
--- js/states/gameoverstate.js
+++ js/states/gameoverstate.js
@@ -41,6 +41,8 @@ MainGame.GameOverState.prototype = {
 			s.anchor.setTo(0.5, 0.5);
 			s.fixedToCamera = true;
 
+					
+
 			if (MainGame.addictingMode === 1) {
 
 				// One-line, "let me play Level2".
